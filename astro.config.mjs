af7316c12606d0a2805b03957acf166b0ef1e475
Flixbox <felix@tietjen.it>
2024-05-10 16:38:47 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			editLink: {
+        baseUrl: 'https://github.com/withastro/starlight/edit/main/docs/',
+      },
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
