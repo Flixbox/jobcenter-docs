0ce544c26765ba6a76b02c338b0d1e6f1e7715ad
Flixbox <felix@tietjen.it>
2024-05-10 16:44:31 +0200

    add deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://flixbox.github.io',
+  	base: 'jobcenter-docs',
 	integrations: [
 		starlight({
 			title: 'My Docs',
